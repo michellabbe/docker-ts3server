f18330db5bdcc2cca68f25b5519f07d1545a63ce
Michel Labbé <michellabbe@users.noreply.github.com>
2016-01-08 16:34:03 -0500

    debug test

--- scripts/getlatest-ts3.sh
+++ scripts/getlatest-ts3.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 #TS_VERSION=`wget --no-check-certificate -qO - https://www.server-residenz.com/tools/ts3versions.json|grep latest|awk '{print $2}'|sed s/\"//g|sed s/,//g`
-TS_VERSION=3.0.11.4
-echo "Installing Teamspeak ${TS_VERSION}"
+#TS_VERSION=3.0.11.4
+#echo "Installing Teamspeak ${TS_VERSION}"
 
-wget --no-check-certificate http://dl.4players.de/ts/releases/$TS_VERSION/teamspeak3-server_linux-amd64-$TS_VERSION.tar.gz \
-         -O /opt/teamspeak3-server_linux-amd64-$TS_VERSION.tar.gz
+#wget http://dl.4players.de/ts/releases/$TS_VERSION/teamspeak3-server_linux-amd64-$TS_VERSION.tar.gz \
+#         -O /opt/teamspeak3-server_linux-amd64-$TS_VERSION.tar.gz
 
 #tar -C /opt -xzf /opt/teamspeak3-server_linux-amd64-$TS_VERSION.tar.gz
 #rm /opt/teamspeak3-server_linux-amd64-$TS_VERSION.tar.gz
